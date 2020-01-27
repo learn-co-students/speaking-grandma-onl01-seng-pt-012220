6676c29b62f4e6f76df138c0a3fe9c9176d9d533
Autumn Taylor <autfall75@hotmail.com>
2020-01-27 18:51:37 +0000

    Done.

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -2,8 +2,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif == phrase.upcase
-    "NO, NOT SINCE 1938"
+    "NO, NOT SINCE 1938!"
   else
-    "HUH? SPEAK UP, SONNY"
+    "HUH? SPEAK UP, SONNY!"
   end
 end
