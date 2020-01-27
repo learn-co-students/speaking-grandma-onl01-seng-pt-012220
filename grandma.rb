e47e82aa6b424e4a6bea0a3b6cc07700c9e55cbd
garrulous-language-0246 <mrlive2k@gmail.com>
2020-01-27 05:47:17 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,12 @@
-# Write a speak_to_grandma method.
+speak_to_grandma = "I LOVE YOU GRANDMA!"
+
+if speak_to_grandma.upcase == "HEY GRANDMA HAVE YOU EVER MOVED TO A DIFFERENT HOME BEFORE?"
+  puts "NO, NOT SINCE 1938!"
+else 
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif speak_to_grandma.upcase = "I LOVE YOU GRANDMA!"
+puts "I LOVE YOU TOO PUMPKIN"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
